b0b1c07df47e5b9b37bb7494b703f32d7fb711c6
Arto-O <arto.olli@live.com>
2023-04-23 19:31:31 -0700

    Change QUOT to SCLN on TYPTEM

--- keyboards/bastardkb/charybdis/3x5/keymaps/artoo/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/artoo/keymap.c
@@ -234,7 +234,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
               L32, L33, L34, L35, L36                             \
     ...)                                                          \
             L00,        L01,        L02,        L03,        L04,  \
-            R05,        R06,        R07,        R08,        R09,  \
+            R05,        R06,        R07,        R08,    KC_SCLN,  \
             L10,        L11,        L12,        L13,        L14,  \
             R15,        R16,        R17,        R18,        R19,  \
             L20,        L21,        L22,        L23,        L24,  \
